c7b6eb8c1b3a7ea7d8c7841136ce7d725f1382aa
JaswantSharma <droidude1@gmail.com>
2022-10-27 17:04:09 +0530

    my first commit

---
+++ EcommerceSolution/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	do {
+		cout << "################################################################" << endl;
+		cout << "########################   Welcome to the Ecommerce ############" << endl;
+		cout << "################################################################" << endl;
+		cout << "\n\n";
+		cout << "Select Menu" << endl;
+		cout << "1. Admin " << endl;
+		cout << "2. Customer " << endl;
+		cout << "3. Exit" << endl;
+		int choice;
+		cin >> choice;
+		if (choice == 1)
+		{
+
+		}
+		else if (choice == 2) {
+
+
+		}
+		else if (choice == 3) {
+			exit(0);
+		}
+		else {
+			cout << "Enter a valid choice" << endl << endl;
+		}
+	} while (1);
+
+
+}
